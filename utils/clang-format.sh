5b70630d2220f503472080f78b02c665a9f09fa5
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2019-11-03 16:30:09 -0500

    add tests directory to code formatter

--- utils/clang-format.sh
+++ utils/clang-format.sh
@@ -4,6 +4,6 @@ cd $(git rev-parse --show-toplevel)
 tmpd=$(mktemp) # diff
 tmpu=$(mktemp) # unformatted
 tmpf=$(mktemp) # formatted
-find src/ -type f -regex '.*\.\(hpp\|cpp\|tpp\)' -print0 \
+find src/ tests/ -type f -regex '.*\.\(hpp\|cpp\|tpp\)' -print0 \
     | xargs -0 -I{} sh -c "echo {}; cat {} > $tmpu; clang-format -style=file {} > $tmpf; diff -u $tmpu $tmpf | tee -a $tmpd; clang-format --style=file -i {}"
 cat $tmpd
