c3cee90cae1ac4b498e4b968d10cf38fb4f33729
Hansel De La Cruz <hansel809@hotmail.com>
2019-11-03 16:08:35 -0500

    allow code formatter to be run from any directory

--- utils/clang-format.sh
+++ utils/clang-format.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env sh
 
-find src/ -regex '.*\.\(hpp\|cpp\|tpp\)' -exec clang-format -style=file -i {} \;
+cd $(git rev-parse --show-toplevel)
+tmpd=$(mktemp) # diff
+tmpu=$(mktemp) # unformatted
+tmpf=$(mktemp) # formatted
+find src/ -type f -regex '.*\.\(hpp\|cpp\|tpp\)' -print0 \
+    | xargs -0 -I{} sh -c "echo {}; cat {} > $tmpu; clang-format -style=file {} > $tmpf; diff -u $tmpu $tmpf | tee -a $tmpd; clang-format --style=file -i {}"
+cat $tmpd
