b78e2d04aa04e5b9e123b7167c790d5222882d93
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2019-11-08 18:45:50 -0500

    add rttr and update extlibs

--- utils/check-include-guards.sh
+++ utils/check-include-guards.sh
@@ -4,11 +4,14 @@ cd $(git rev-parse --show-toplevel)
 tmpd=$(mktemp) # diff
 tmpb=$(mktemp) # before
 tmpa=$(mktemp) # after
-find src/ tests/ -type f -regex '.*\.\(hpp\|cpp\|tpp\)' -print0 |
+find src/ tests/ -type f -regex '.*\.\(hpp\)' -print0 |
     while IFS= read -r -d '' file; do
         echo $file
+        cur_ig=$(cat $file | grep "#ifndef" | head -n 1 | cut -d ' ' -f 2)
+        new_ig=$(echo $file | cut -d "/" -f 2- | tr ./ _ | tr [a-z] [A-Z])
         cat $file > $tmpb
-        clang-format -style=file $file > $tmpa
+        cat $file > $tmpa
+        sed -i "s/$cur_ig/$new_ig/g" $tmpa
         diff -u $tmpb $tmpa | tee -a $tmpd
     done
 cat $tmpd
