57028bc004fc1317d7a59baf1bd2879c169b9aa9
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2020-03-28 13:27:30 -0400

    add common macros

--- src/binclient/main.cpp
+++ src/binclient/main.cpp
@@ -1,6 +1,10 @@
 #include <ph_common.hpp>
 
-int main() {
+int main(int argc, char *argv[]) {
+    PH_UNUSED(argc);
+    PH_UNUSED(argv);
+
     ph::common::log::init("ph_client.log");
+
     return 0;
 }
