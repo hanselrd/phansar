cb0de1826e9fc072159d7316fdd275fb151bad79
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2020-03-29 02:55:50 -0400

    add assert, expect and unused macros

--- src/libcommon/macros/include/common_macros_expect.hpp
+++ src/libcommon/macros/include/common_macros_expect.hpp
@@ -1,9 +1,28 @@
 #ifndef LIBCOMMON_MACROS_INCLUDE_COMMON_MACROS_EXPECT_HPP
 #define LIBCOMMON_MACROS_INCLUDE_COMMON_MACROS_EXPECT_HPP
 
+#include "common_log.hpp"
 #include <plibsys.h>
 
-#define EXPECT_TRUE P_LIKELY
-#define EXPECT_FALSE P_UNLIKELY
+#ifndef NDEBUG
+#    define EXPECT_TRUE(condition)                                                                 \
+        [&] {                                                                                      \
+            if (!(condition)) {                                                                    \
+                LOGW("Expected `" #condition "' to be true");                                      \
+            }                                                                                      \
+            return condition;                                                                      \
+        }()
+
+#    define EXPECT_FALSE(condition)                                                                \
+        [&] {                                                                                      \
+            if (condition) {                                                                       \
+                LOGW("Expected `" #condition "' to be false");                                     \
+            }                                                                                      \
+            return condition;                                                                      \
+        }()
+#else
+#    define EXPECT_TRUE(condition) P_LIKELY(condition)
+#    define EXPECT_FALSE(condition) P_UNLIKELY(condition)
+#endif
 
 #endif
--- src/libcommon/system/common_system.cpp
+++ src/libcommon/system/common_system.cpp
@@ -5,10 +5,11 @@
 
 namespace common::system {
 void init(int argc, char **argv) {
-    UNUSED(argc);
-
     auto bin = std::string{argv[0]};
 
     log::init(bin.substr(bin.find_last_of('/') + 1) + ".log");
+
+    // postponed until after we initialize logger
+    UNUSED_ARG(argc);
 }
 } // namespace common::system
