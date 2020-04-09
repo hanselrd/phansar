bb463cc6811979a186415ce4ba69cba41eefa85d
Hansel De La Cruz <hansel809@hotmail.com>
2020-04-09 18:54:08 -0400

    add socket address stub

---
+++ src/libcommon/networking/common_networking_socket_address.cpp
@@ -0,0 +1,3 @@
+#include "common_networking_socket_address.hpp"
+
+namespace common::networking {}
---
+++ src/libcommon/networking/common_networking_socket_address.unittest.cpp
@@ -0,0 +1,6 @@
+#include "common_networking_socket_address.hpp"
+#include <catch2/catch.hpp>
+
+TEST_CASE("can use socket address", "[libcommon][networking][socket_address]") {
+    /* auto sa = common::networking::socket_address{}; */
+}
