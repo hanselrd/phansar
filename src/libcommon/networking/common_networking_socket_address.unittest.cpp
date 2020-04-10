f4e25d831b690ac0e085fc68edc7fa298887c452
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2020-04-10 03:53:42 -0400

    update unit tests

--- src/libcommon/networking/common_networking_socket_address.unittest.cpp
+++ src/libcommon/networking/common_networking_socket_address.unittest.cpp
@@ -1,6 +1,4 @@
 #include "common_networking_socket_address.hpp"
 #include <catch2/catch.hpp>
 
-TEST_CASE("can use socket address", "[libcommon][networking][socket_address]") {
-    /* auto sa = common::networking::socket_address{}; */
-}
+TEST_CASE("common_networking_socket_address", "[common][networking][socket_address]") {}
--- src/libcommon/threading/common_threading_thread_pool.unittest.cpp
+++ src/libcommon/threading/common_threading_thread_pool.unittest.cpp
@@ -5,28 +5,30 @@
 
 #define THREADING_THREAD_POOL_UNITTEST_MAX_ITERATIONS (10000)
 
-TEST_CASE("can use thread pool", "[libcommon][threading][thread_pool]") {
-    auto tp         = common::threading::thread_pool{};
-    auto count      = std::atomic_uint64_t{0};
-    auto futs       = std::vector<std::future<std::uint64_t>>{};
-    auto futs_count = std::uint64_t{0};
+TEST_CASE("common_threading_thread_pool", "[common][threading][thread_pool]") {
+    SECTION("can use thread pool") {
+        auto tp         = common::threading::thread_pool{};
+        auto count      = std::atomic_uint64_t{0};
+        auto futs       = std::vector<std::future<std::uint64_t>>{};
+        auto futs_count = std::uint64_t{0};
 
-    for (auto i = std::size_t{0}; i < THREADING_THREAD_POOL_UNITTEST_MAX_ITERATIONS; ++i) {
-        tp.push_work([&count] { count.fetch_add(1); });
+        for (auto i = std::size_t{0}; i < THREADING_THREAD_POOL_UNITTEST_MAX_ITERATIONS; ++i) {
+            tp.push_work([&count] { count.fetch_add(1); });
 
-        auto fut = tp.push_task([&count] {
-            count.fetch_add(1);
-            return std::uint64_t{1};
-        });
-        futs.push_back(std::move(fut));
+            auto fut = tp.push_task([&count] {
+                count.fetch_add(1);
+                return std::uint64_t{1};
+            });
+            futs.push_back(std::move(fut));
+        }
+
+        for (auto & f : futs) {
+            futs_count += f.get();
+        }
+
+        tp.wait_done();
+
+        REQUIRE(count.load() == (2 * THREADING_THREAD_POOL_UNITTEST_MAX_ITERATIONS));
+        REQUIRE(futs_count == THREADING_THREAD_POOL_UNITTEST_MAX_ITERATIONS);
     }
-
-    for (auto & f : futs) {
-        futs_count += f.get();
-    }
-
-    tp.wait_done();
-
-    REQUIRE(count.load() == (2 * THREADING_THREAD_POOL_UNITTEST_MAX_ITERATIONS));
-    REQUIRE(futs_count == THREADING_THREAD_POOL_UNITTEST_MAX_ITERATIONS);
 }
