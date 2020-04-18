c400859cf8b8f62d2c960956e8ee589029a1628a
Hansel De La Cruz <hansel809@hotmail.com>
2020-04-18 14:40:49 -0400

    add shell formatter

--- utils/beautysh.bash
+++ utils/beautysh.bash
@@ -2,7 +2,7 @@
 
 cd "$(git rev-parse --show-toplevel)" || exit
 
-tmpdir=$(mktemp -d -p /tmp include-guards-XXXXXXXXX)
+tmpdir=$(mktemp -d -p /tmp beautysh-XXXXXXXXX)
 tmpf=$(mktemp -p "$tmpdir" XXXXXXXXX.fdiff)
 
 check() {
@@ -10,24 +10,21 @@ check() {
     tmpb=$(mktemp -p "$tmpdir" XXXXXXXXX.before)
     tmpa=$(mktemp -p "$tmpdir" XXXXXXXXX.after)
     echo "$1"
-    cur_ig=$(grep "#ifndef" < "$1" | head -n 1 | cut -d ' ' -f 2)
-    new_ig=$(echo "$1" | cut -d '/' -f 2- | tr ./ _ | tr "[:lower:]" "[:upper:]")
     cat "$1" > "$tmpb"
-    cat "$1" > "$tmpa"
-    sed -i "s/$cur_ig/$new_ig/g" "$tmpa"
+    beautysh -i 4 -s paronly - < "$1" > "$tmpa"
     diff -u "$tmpb" "$tmpa" | tee -a "$tmpd"
 }
 
 fix() {
     check "$1"
-    sed -i "s/$cur_ig/$new_ig/g" "$1"
+    beautysh -i 4 -s paronly "$1"
 }
 
 ### MAIN
 
 case "$1" in
     check)
-        find src tests vendor -type f -regex '.*\.\(hpp\)$' -print0 |
+        find utils -type f -regex '.*\.\(sh\|bash\)$' -print0 |
         while IFS= read -r -d '' file; do
             check "$file"
         done
@@ -38,7 +35,7 @@ case "$1" in
         done
         ;;
     fix)
-        find src tests vendor -type f -regex '.*\.\(hpp\)$' -print0 |
+        find utils -type f -regex '.*\.\(sh\|bash\)$' -print0 |
         while IFS= read -r -d '' file; do
             fix "$file"
         done
