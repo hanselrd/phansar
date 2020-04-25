3c6c238ba0495b6220fc756ff830c80e8d177d39
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2020-04-24 23:05:12 -0400

    add private vendor headers

--- include/phansar/vendor/spdlog.hpp
+++ include/phansar/vendor/spdlog.hpp
@@ -7,10 +7,7 @@
 #    pragma warning(push, 0)
 #endif
 
-#include <spdlog/common.h>
 #include <spdlog/fmt/ostr.h>
-#include <spdlog/sinks/rotating_file_sink.h>
-#include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/spdlog.h>
 
 #ifdef HEDLEY_MSVC_VERSION
