0d7bf98b13511ee1c6a9a2d433b4c762f0da0726
Hansel De La Cruz <hansel809@hotmail.com>
2020-10-22 15:14:10 -0400

    add update_vendors script

---
+++ tools/helpers/shell.py
@@ -0,0 +1,52 @@
+import logging
+import shutil
+import subprocess
+import time
+import typing
+
+def shell(
+    cmd: str,
+    stdout: bool = True,
+    stderr: bool = True,
+    dryrun: bool = False,
+) -> typing.Tuple[int, typing.Optional[str], typing.Optional[str]]:
+    if dryrun:
+        logging.info(f"(DRYRUN) cmd=`{repr(cmd)}`")
+        return 0, None, None
+    else:
+        logging.info(f"cmd=`{repr(cmd)}`")
+        process = subprocess.Popen(
+            cmd,
+            universal_newlines=True,
+            shell=True,
+            stdout=subprocess.PIPE if stdout else None,
+            stderr=subprocess.PIPE if stderr else None,
+        )
+        if stdout:
+            start = time.time()
+            while True:
+                try:
+                    stdout, stderr = process.communicate(timeout=1)
+                    break
+                except subprocess.TimeoutExpired:
+                    elapsed = int(time.time() - start)
+                    if elapsed >= 1:
+                        columns, _ = shutil.get_terminal_size()
+                        progress = "=" * (elapsed % ((columns // 2) - 15))
+                        print(
+                            f"<{progress} {elapsed}s {progress}>".center(columns),
+                            end="\r",
+                            flush=True,
+                        )
+                    continue
+        else:
+            stdout, stderr = process.communicate()
+        if stdout:
+            logging.info(f"  stdout=`{repr(stdout)}`")
+        if stderr:
+            logging.info(f"  stderr=`{repr(stderr)}`")
+        return (
+            process.returncode,
+            stdout.strip() if stdout else None,
+            stderr.strip() if stderr else None,
+        )
---
+++ tools/update_vendors.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+
+from helpers.shell import shell
+import argparse
+import glob
+import logging
+import os
+import re
+import sys
+import tempfile
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Update vendor libraries",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        add_help=False,
+    )
+
+    # positional_parser = parser.add_argument_group("positional arguments")
+
+    # required_parser = parser.add_argument_group("required arguments")
+
+    optional_parser = parser.add_argument_group("optional arguments")
+    optional_parser.add_argument(
+        "-h",
+        "--help",
+        help="show this help message and exit",
+        action="help",
+        default=argparse.SUPPRESS,
+    )
+    optional_parser.add_argument(
+        "-l",
+        "--log-level",
+        help="set logging level",
+        choices=[
+            logging.NOTSET,
+            logging.DEBUG,
+            logging.INFO,
+            logging.WARNING,
+            logging.ERROR,
+            logging.CRITICAL,
+        ],
+        default=logging.DEBUG,
+    )
+
+    args = parser.parse_args()
+
+    logging.basicConfig(
+        stream=sys.stdout,
+        format="%(asctime)s  %(levelname)-8s  |  (%(funcName)s)  %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S.%e %z",
+        level=args.log_level,
+    )
+
+    os.chdir(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+    files = [f for f in glob.glob('vendor/*/CMakeLists.txt', recursive=True) if
+             os.path.isfile(f)]
+
+    for f in files:
+        with tempfile.TemporaryDirectory() as tmpdir:
+            with open(f, 'r') as file:
+                data = file.read()
+                groups = re.search('GIT_REPOSITORY\s*(.*)\s*GIT_TAG\s*(.*)\)\s*.*\s*.*\s*.*\s*check_git_commit_hash\((?:\w|-|\.)*\s*\${\w*}\s*(.*)\)',data).groups()
+                shell(f"git clone {groups[0]} -b {groups[2]} {tmpdir}")
+                shell(f"cd {tmpdir} && git checkout {groups[1]}")
+                _, stdout, _ = shell(f"cd {tmpdir} && git rev-parse HEAD")
+                HEAD_HASH = stdout.split("\n")[0]
+                _, stdout, _ = shell(f"cd {tmpdir} && git rev-parse origin/{groups[2]}")
+                ORIGIN_HASH = stdout.split("\n")[0]
+
+            if HEAD_HASH != ORIGIN_HASH:
+                data = data.replace(groups[1], ORIGIN_HASH)
+
+                with open(f, 'w') as file:
+                    file.write(data)
