5a46baa1a08214892c06f2824493ec9904b79457
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2021-02-12 17:08:48 -0500

    add nonsynchronizable

---
+++ include/phansar/common/utility/nonsynchronizable.hpp
@@ -0,0 +1,8 @@
+#ifndef PHANSAR_COMMON_UTILITY_NONSYNCHRONIZABLE_HPP
+#define PHANSAR_COMMON_UTILITY_NONSYNCHRONIZABLE_HPP
+
+namespace phansar::common::utility {
+class nonsynchronizable {};
+} // namespace phansar::common::utility
+
+#endif
