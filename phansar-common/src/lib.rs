2e136a324e1ec6125fa5b1363dff7eba6348d6be
Hansel De La Cruz <18725263+hanselrd@users.noreply.github.com>
2021-07-17 21:12:58 -0400

    add common application

--- phansar-common/src/lib.rs
+++ phansar-common/src/lib.rs
@@ -1,7 +1,15 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+// #[derive(
+//     Debug, Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, derive_builder::Builder,
+// )]
+// #[builder(setter(into))]
+
+pub mod application;
+pub use application::*;
+
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() {
+//         assert_eq!(2 + 2, 4);
+//     }
+// }
